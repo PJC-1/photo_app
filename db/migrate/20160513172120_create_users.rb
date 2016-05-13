10daa682e13c0e4694f4fe7702f263141bdf55e1
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-13 11:58:06 -0700

    updated user migration with devise

--- db/migrate/20160513172120_create_users.rb
+++ db/migrate/20160513172120_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password_digest
       t.string :profile_picture
 
       t.timestamps null: false
