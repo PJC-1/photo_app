3d6a029a655580be86c56f1bccc5e0ca48396f74
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-13 13:54:47 -0700

    devise views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @users = User.find_by_id([:id])
+    render :show
   end
 end
