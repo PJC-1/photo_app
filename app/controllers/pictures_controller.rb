3899400a2e07295495def0372c5d6dc54ab12bdb
Herman Chan <hermchan89@gmail.com>
2016-05-16 13:58:56 -0700

    commit before pulling from development

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,17 +14,16 @@ class PicturesController < ApplicationController
 
     def create
       @picture = Picture.create(picture_params)
-      Cloudinary::Uploader.upload(params[:picture][:pic_img])
-                                  # :public_id => "",
-                                  # :crop => :limit, :width => 2000, :height => 2000,
-                                  # :eager => [
-                                  #   { :width => 200, :height => 200,
-                                  #     :crop => :thumb, :gravity => :face,
-                                  #     :radius => 20, :effect => :sepia },
-                                  #   { :width => 100, :height => 150,
-                                  #     :crop => :fit, :format => 'png'}
-                                  #   ],
-                                  #   :tags => ['special', 'for_homepage'])
+      Cloudinary::Uploader.upload(params[:picture][:pic_img],
+                                  :crop => :limit, :width => 2000, :height => 2000,
+                                  :eager => [
+                                    { :width => 200, :height => 200,
+                                      :crop => :thumb, :gravity => :face,
+                                      :radius => 20, :effect => :sepia },
+                                    { :width => 100, :height => 150,
+                                      :crop => :fit, :format => 'png'}
+                                    ],
+                                    :tags => ['special', 'for_homepage'])
       current_user.pictures << @picture
 
       redirect_to pictures_path
