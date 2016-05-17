0707223921c4a3a374be6440a4ccff9f6338f116
PJC-1 <jumpzine@gmail.com>
2016-05-17 15:33:15 -0700

    commit before pulling from master

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,4 +5,28 @@ class Picture < ActiveRecord::Base
   has_many :comments
   has_many :picture_tags
   has_many :tags, through: :picture_tags
+
+  # returns pictures with that tag name
+  def self.tagged_wtih(name)
+    Tag.find_by_name!(name).pictures
+  end
+
+  # fetches all tags with a count
+  def self.tag_counts
+    Tag.select("tags.*, count(picture_tags.tag_id) as count").
+    joins(:picture_tags).group("picture_tags.tag_id")
+  end
+
+  # returns list of the the tags
+  def tag_list
+    tags.map(&:name).join(", ")
+  end
+
+  # sets a list of tags
+  def tag_list=(names)
+    self.tags = names.split(",").map do |n|
+      Tag.where(name: n.strip).first_or_create!
+    end
+  end
+
 end
