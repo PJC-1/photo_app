ef6ce6abe676cdf80c3555d690d0afe0e12abe05
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-17 19:55:31 -0700

    you can now add your own avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :profile_picture, ProfilePictureUploader
+
   has_many :pictures
   has_many :comments
 end
